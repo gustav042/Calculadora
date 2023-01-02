2c728a333e7340f288b55117eb66cc3bebfb57ab
gustav042 <gustavofelipereira42@gmail.com>
2023-01-02 11:54:21 -0300

    preciso fazer aceitar numeros com mais de 1 digito

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 var visor = window.document.querySelector('div.visor')
 var num1
 var num2
-num1=2
-num2=3
-visor.innerHTML = num1+num2
+var first = 0
+
+
+document.querySelectorAll('input').forEach(el => {
+    el.addEventListener('click', () => {
+        digit_pressed(el.value);
+    });
+});
+function digit_pressed(digit) {
+    if (digit == '=') {
+        visor.innerHTML = "";
+        visor.innerHTML = Number(num1) + Number(num2);
+    }
+    else if (digit == 'DEL') {
+        visor.innerHTML = "";
+        first = 0;
+    }
+    else if (digit == '/' || digit == 'x' || digit == '-' || digit == '+')
+    {
+        first = 1
+        visor.innerHTML += digit;
+
+    }
+    else {
+        if (first == 0)
+        {
+            num1 += digit;
+        }
+        else
+        {
+            num2 += digit;
+        }
+        visor.innerHTML += digit;
+
+    }
+}
