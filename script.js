1c0eaa9cf1b208c657fa5abfb35925e52c94b0ff
gustav042 <gustavofelipereira42@gmail.com>
2023-01-05 07:58:01 -0300

    fiz todas as operações, falta fazer o botão de apagar ultimo digito escrito

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var visor = window.document.querySelector('div.visor')
-var num1
-var num2
+var num1 = ''
+var num2 = ''
+var op = ''
 var first = 0
 
 
@@ -9,18 +10,37 @@ document.querySelectorAll('input').forEach(el => {
         digit_pressed(el.value);
     });
 });
+
 function digit_pressed(digit) {
     if (digit == '=') {
         visor.innerHTML = "";
-        visor.innerHTML = Number(num1) + Number(num2);
+        switch(op) {
+            case '+':
+                visor.innerHTML = Number(num1) + Number(num2);
+                break;
+            case '-':
+                visor.innerHTML = Number(num1) - Number(num2);
+                break;
+            case '/':
+                visor.innerHTML = Number(num1) / Number(num2);
+                break;
+            default:
+                visor.innerHTML = Number(num1) * Number(num2);
+          }
+        num1 = ''
+        num2 = ''
     }
     else if (digit == 'DEL') {
         visor.innerHTML = "";
         first = 0;
+        num1 = ''
+        num2 = ''
+        
     }
-    else if (digit == '/' || digit == 'x' || digit == '-' || digit == '+')
+    else if (digit == '/' || digit == 'x' || (digit == '-' && num1 != '' )|| digit == '+')
     {
         first = 1
+        op = digit;
         visor.innerHTML += digit;
 
     }
@@ -34,6 +54,6 @@ function digit_pressed(digit) {
             num2 += digit;
         }
         visor.innerHTML += digit;
-
     }
+
 }
